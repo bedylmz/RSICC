07e7677ebc4028f73562af5715da097d4bf4e44c
bedylmz <bedylmz03@gmail.com>
2025-12-23 15:13:16 +0300

    eval fixed

--- eval.py
+++ eval.py
@@ -7,6 +7,7 @@ import torch.nn.functional as F
 from tqdm import tqdm
 import argparse
 import time
+
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                              std=[0.229, 0.224, 0.225])
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
