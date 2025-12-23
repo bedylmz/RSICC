9d91a544962a2e605d1db3ed62ef745a9eefeb3d
bedylmz <bedylmz03@gmail.com>
2025-12-23 15:17:21 +0300

    eval fixed

--- eval.py
+++ eval.py
@@ -385,6 +385,7 @@ if __name__ == '__main__':
     parser.add_argument('--data_name', default="LEVIR_CC_5_cap_per_img_5_min_word_freq",help='base name shared by data files.')
 
     parser.add_argument("--clip_path", type=str, default="/content/RSICC/ckpts/pytorch_model.bin.0", help="Layer NO. of intra module")
+    parser.add_argument("--cross_model", default="cross-base", type=str, required=False, help="Cross module")
 
     parser.add_argument('--encoder_image', default="resnet101")
     parser.add_argument('--encoder_feat', default="MCCFormers_diff_as_Q")
