a504b14866ce65c567b8dc5e13429d5457e5d90f
bedylmz <bedylmz03@gmail.com>
2026-01-11 13:36:51 +0300

    clip updated

--- train.py
+++ train.py
@@ -1193,7 +1193,6 @@ if __name__ == "__main__":
 
     parser.add_argument("--pretrained_clip_name", default="ViT-B/32", type=str, help="Choose a CLIP version")
 
-    parser.add_argument("--cross_model", default="cross-base", type=str, required=False, help="Cross module")
     parser.add_argument("--task_type", default="retrieval", type=str, help="Point the task `retrieval` to finetune.")
     parser.add_argument("--intra_num_hidden_layers", type=int, default=9, help="Layer NO. of intra module")
     parser.add_argument("--clip_path", type=str, default="/content/RSICC/ckpts/pytorch_model.bin.0", help="Layer NO. of intra module")
