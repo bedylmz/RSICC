03b6b571096a2260ba588433a722d8ab52b306b0
bedylmz <bedylmz03@gmail.com>
2026-01-25 18:11:27 +0300

    logger error

--- train3.py
+++ train3.py
@@ -250,6 +250,20 @@ class GatedSelfAttention(nn.Module):
         
         return output, attention
     
+import logging
+def get_logger(filename=None):
+    logger = logging.getLogger('logger')
+    logger.setLevel(logging.DEBUG)
+    logging.basicConfig(format='%(asctime)s - %(levelname)s -   %(message)s',
+                    datefmt='%m/%d/%Y %H:%M:%S',
+                    level=logging.INFO)
+    if filename is not None:
+        handler = logging.FileHandler(filename)
+        handler.setLevel(logging.DEBUG)
+        handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s: %(message)s'))
+        logging.getLogger().addHandler(handler)
+    return logger
+
 def train(
     args= None,
     train_loader= None,
